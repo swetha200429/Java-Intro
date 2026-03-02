de6945027fd5bb691f4a6f5e43d8b36a9adfd6ab
swetha200429 <swetha200429@gmail.com>
2026-03-02 21:05:31 +0530

    Create BankAccountSimulation.java

---
+++ BankAccountSimulation.java
@@ -0,0 +1,14 @@
+class BankAccountSimulation{
+  public static void main(String[] args){
+    String accountHoldername="Swetha";
+    int accountNumber=123456789;
+    double accountBalance=50000;
+    double depositeamount=10000;
+    double withdrawAmount=7000;
+    accountBalance+=depositeAmount;
+    accountBalance-=withdrawAmount;
+    System.out.println("Account Holder:"+accountHoldername);
+    System.out.println("Account Number:"+accountNumber);
+    System.out.println("FInal Balance:"+accountBalance);
+  }
+}
