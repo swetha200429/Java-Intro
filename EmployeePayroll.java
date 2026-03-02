1114a3f4dd8615571124b18eba4b26a85f0e39f8
swetha200429 <swetha200429@gmail.com>
2026-03-02 20:56:39 +0530

    Create EmployeePayroll.java

---
+++ EmployeePayroll.java
@@ -0,0 +1,16 @@
+public class EmployeePayroll{
+  public static void main(String[] args){
+    String name="Ravi";
+    int empId=1001;
+    double basicSalary=4000;
+    double hra=8000;
+    double da=5000;
+    double tax=3000;
+    double grossSalary=basicSalary+hra+da;
+    double netSalary=grossSalary-tax;
+    System.out.println("Employee Name:"+name);
+    System.out.println("Employee ID:"+empID);
+    System.out.println("Gross ID:"+grossSalary);
+    System.out.println("Net Salary:"+netSalary);
+  }
+}
