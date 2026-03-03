aafa58313b86e63f3626c081673a86a5c9b383a3
swetha200429 <swetha200429@gmail.com>
2026-03-03 11:31:03 +0530

    Create ExamEligibility.java

---
+++ ExamEligibility.java
@@ -0,0 +1,12 @@
+class ExamEligibility{
+  public static void main(String[] args){
+    int attendance=82;
+    boolean feesPaid=true;
+    boolean blacklisted=false;
+    boolean isEligibily=attendance>=75 && feespaid && !blacklisted;
+    System.out.println("Attendance:"+attendance+"%");
+    System.out.println("Fees Paid:"+feespaid);
+    System.out.println("BlackListed:"+blacklisted);
+    System.out.println("Eligible for Exam:"+isEligivle);
+  }
+}
