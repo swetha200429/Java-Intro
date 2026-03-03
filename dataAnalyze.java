ac7aa12c45646f4f46dbb78a74d3277ae6945b02
swetha200429 <swetha200429@gmail.com>
2026-03-03 20:17:07 +0530

    Create dataAnalyze.java

---
+++ dataAnalyze.java
@@ -0,0 +1,15 @@
+class DataAnalyze{
+  public static void main(String[] args){
+    int number=50;
+    double decimal=12.75;
+    char grade='A';
+    boolean status=true;
+    int squared=number*number;
+    double half=decimal/2;
+    System.out.println("Number:"+number);
+    System.out.println("Squared:"+squared);
+    System.out.println("Decimal Half:"+half);
+    System.out.println("Grade:"+grade);
+    System.out.println("Status:"+status);
+  }
+}
