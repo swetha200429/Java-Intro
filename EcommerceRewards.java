83c7e9d14027da1e6695e771b2a70f9a66f54b01
swetha200429 <swetha200429@gmail.com>
2026-03-03 20:59:31 +0530

    Create EcommerceRewards.java

---
+++ EcommerceRewards.java
@@ -0,0 +1,11 @@
+class EcommerceRewards{
+  public static void main(String[] args){
+    double purchaseAmount=1250.75;
+    double cashbackrate=0.05;
+    double cashback=purchaseAmount*cashbackRate;
+    int rewardPoints=(int)cashback;
+    System.out.println("Purchase Amount:"+PurchaseAmount);
+    System.out.println("Cashback Earned:"+cashback);
+    System.out.println("Reward Points (int stored):"+rewardPoints);
+  }
+}
