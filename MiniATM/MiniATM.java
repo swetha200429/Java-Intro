23af1456f3341d23ac2a20df9c12cc1a1bf1a12e
swetha200429 <swetha200429@gmail.com>
2026-03-03 09:58:29 +0530

    Create MiniATM.java

---
+++ MiniATM/MiniATM.java
@@ -0,0 +1,22 @@
+class MiniATM{
+  public static void main(String[] args){
+    int enteredPin=1234;
+    int correctPin=1234;
+    double balance=25000;
+    double withdrawAmount=5000;
+    if(enteredPin==correctPin){
+      System.out.println("Login Successful!");
+      if(withdrawAmount<=balance){
+        balance-=withdrawAmount;
+        System.out.println("withdrawal Successful!");
+      }
+      else{
+        System.out.println("Insufficient Balance!");
+      }
+      System.out.println("Remaining Balance:"+balance);
+    }
+    else{
+      System.out.println("Invalid PIN!");
+    }
+  }
+}
