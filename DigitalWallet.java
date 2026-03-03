cd0c02bc6de88ed99f9d2b05282d3f7e3c60bc24
swetha200429 <swetha200429@gmail.com>
2026-03-03 21:41:23 +0530

    Create DigitalWallet.java

---
+++ DigitalWallet.java
@@ -0,0 +1,11 @@
+class DigitalWallet{
+  public static void main(String[] args){
+    double balance=5000;
+    balance+=2000;
+    balance-=1500;
+    balance+=200;
+    balance*=1.02;
+    balance-=100;
+    System.out.println("Final Wallet Balance:"+balance);
+  }
+}
