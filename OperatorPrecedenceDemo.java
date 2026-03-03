9b63e87468a7fc1d9c4339c77166391854685eb4
swetha200429 <swetha200429@gmail.com>
2026-03-03 22:16:06 +0530

    Create OperatorPrecedenceDemo.java

---
+++ OperatorPrecedenceDemo.java
@@ -0,0 +1,10 @@
+class OperatorPrecedenceDemo{
+  public static void main(String[] args){
+    int result1=10+5*2;
+    int result2=(10+5)*2;
+    int result3=100/5+20*2;
+    System.out.println("10+5+2="+result1);
+    System.out.println("(10+5)*2="+result2);
+    System.out.println("100/5+20*2="+result3);
+  }
+}
