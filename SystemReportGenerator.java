376eb219b5d8e0f7928c0ef1bc10eb2b48cea26f
swetha200429 <swetha200429@gmail.com>
2026-03-04 19:03:46 +0530

    Create SystemReportGenerator.java

---
+++ SystemReportGenerator.java
@@ -0,0 +1,13 @@
+class SystemReportGenerator{
+  public static void main(String[] args){
+    String report = "System Report\n" +
+                        "----------------------\n" +
+                        "User Name:\t\"Swetha\"\n" +
+                        "Login Status:\tSuccess\n" +
+                        "File Path:\tC:\\Users\\Swetha\\Documents\\report.txt\n" +
+                        "----------------------\n" +
+                        "Report Generated Successfully!\n";
+
+    System.out.println(report);
+  }
+}
